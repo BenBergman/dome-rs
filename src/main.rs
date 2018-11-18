070f8d38da262c9506197aa4c74263b6f1a6297e
Ben Bergman <ben@benbergman.ca>
2018-11-18 11:26:39 -0600

    Displaying wrapping counter value now

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ extern crate panic_abort;
 use hal::clock::GenericClockController;
 use hal::delay::Delay;
 use hal::prelude::*;
+use hal::time::MegaHertz;
 use hal::{entry, CorePeripherals, Peripherals};
 
 #[entry]
@@ -23,10 +24,31 @@ fn main() -> ! {
     let mut pins = hal::Pins::new(peripherals.PORT);
     let mut red_led = pins.d13.into_open_drain_output(&mut pins.port);
     let mut delay = Delay::new(core.SYST, &mut clocks);
+
+    let mut latch = pins.d2.into_open_drain_output(&mut pins.port);
+
+    let mut spi = hal::spi_master(
+        &mut clocks,
+        MegaHertz(10),
+        peripherals.SERCOM4,
+        &mut peripherals.PM,
+        pins.sck,
+        pins.mosi,
+        pins.miso,
+        &mut pins.port,
+    );
+
+    let mut counter = 0xFA;
+
     loop {
         delay.delay_ms(200u8);
         red_led.set_high();
         delay.delay_ms(200u8);
         red_led.set_low();
+
+        let _ = spi.send(counter);
+        latch.set_low();
+        latch.set_high();
+        counter = counter.wrapping_add(1);
     }
 }
